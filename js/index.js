2389e7d99f4feb50cd9bfe839742b1d409322287
shinephopo <shine_phopo@naver.com>
2022-02-27 23:37:43 +0900

    banner typing effect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log{"test"}
+//타이핑 효과, 한글자씩 등장
+const content = "디자이너와 소통을 원활하게 할 수 있는 신입 퍼블리셔 신은지입니다."
+const text = document.querySelector(".text")
+let index = 0;
+
+function typing(){
+    text.textContent += content [index++];
+    if(index > content.length){
+        text.textContent=""
+        index = 0;
+    }
+}
+// 타이핑 속도
+setInterval(typing, 300)
