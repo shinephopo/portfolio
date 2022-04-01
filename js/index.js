3964d200ef89808ef8db256ac7645fed8f1f2b9d
shinephopo <shine_phopo@naver.com>
2022-04-02 03:14:04 +0900

    반응형  pc버전

--- js/index.js
+++ js/index.js
@@ -76,4 +76,40 @@ $(document).ready(function() {
               el: ".swiper-pagination",
               clickable: true,
             },
+            breakpoints:{
+              550:{
+                slidesPerView:2,
+                spaceBetween:10
+              },
+              1100:{
+                slidesPerView:2,
+                spaceBetween:10
+              }
+            }
           });
+
+          //graphic swiper demos
+        var swiper = new Swiper(".graphicSwiper", {
+          slidesPerView: 1,
+          spaceBetween: 10,
+          freeMode: true,
+          cssMode: true,
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          breakpoints:{
+            550:{
+              slidesPerView:1,
+              spaceBetween:10
+            },
+            1000:{
+              slidesPerView:4,
+              spaceBetween:10
+            }
+          }
+        });
